ea8e63072992616fe5ef4458969bcfb5cc83c2bc
BanzaiNamco <114381096+BanzaiNamco@users.noreply.github.com>
2023-02-27 19:13:41 +0800

    fixed view

--- MazeView.java
+++ MazeView.java
@@ -1,15 +1,23 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class MazeView {
     private JFrame frame;
     private MazePanel panel;
+    private JPanel panel2;
     private JButton startButton;
     private JLabel nodesVisited;
+    private JLabel status;
     private int mazeSize;
     private int frameSize = 900;
 
@@ -21,12 +29,22 @@ public class MazeView {
         frame.setLayout(new BorderLayout());
         frame.setLocationRelativeTo(null); 
         panel = new MazePanel(maze, frameSize, mazeSize);
+        panel2 = new JPanel(new GridLayout(1,3, -1, -1));
+        panel2.setBorder(BorderFactory.createEmptyBorder(10, 2, 10, 2));
         startButton = new JButton("Run");
-        startButton.setSize(50, 35);
-        nodesVisited = new JLabel("0 nodes visited");
-        nodesVisited.setSize(50, 20);
-        frame.add(startButton,BorderLayout.SOUTH);
-        frame.add(nodesVisited,BorderLayout.NORTH);
+        startButton.setBorder(BorderFactory.createLineBorder(Color.black));
+        startButton.setFocusPainted(false);
+        startButton.setPreferredSize(new Dimension(45,45));
+        nodesVisited = new JLabel("0 states visited", SwingConstants.CENTER);
+        nodesVisited.setPreferredSize(new Dimension(45,45));
+        nodesVisited.setBorder(BorderFactory.createLineBorder(Color.black));
+        status = new JLabel("", SwingConstants.CENTER);
+        status.setBorder(BorderFactory.createLineBorder(Color.black));
+        status.setPreferredSize(new Dimension(45,45));
+        panel2.add(startButton);
+        panel2.add(status);
+        panel2.add(nodesVisited);
+        frame.add(panel2,BorderLayout.NORTH);
         frame.add(panel, BorderLayout.CENTER);
         frame.setVisible(true);
     }
@@ -40,6 +58,14 @@ public class MazeView {
     }
 
     public void setNodesVisitedTxt(String txt){
-        nodesVisited.setText(txt + " nodes visited");
+        nodesVisited.setText(txt + " states visited");
+    }
+
+    public void setGoalNotFoundTxt(){
+        status.setText("Goal not found...");
+    }
+
+    public void setGoalFound(){
+        status.setText("Goal found!");
     }
 }
